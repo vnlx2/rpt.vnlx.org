0e1525d7e56f5d913533b4343822fd6879023c0a
Ammar Faizi <ammarfaizi2@gnuweeb.org>
2024-09-07 21:35:43 +0700

    Split udata and add funding from Pandika
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

--- public/cf_2024_nov.php
+++ public/cf_2024_nov.php
@@ -1,66 +1,7 @@
 <?php
 
 require __DIR__."/../src/helpers.php";
-
-$fields = [
-	"date",
-	"desc",
-	"in",
-	"out",
-	"balance"
-];
-$potp = "assets/img/pot";
-$data = [
-	[
-		"2024-08-24 22:25:00",
-		"Funding from Ammar Faizi",
-		300000,
-		0,
-		""
-	],
-	[
-		"2024-08-24 22:25:00",
-		"Bayar booth CF ke M. Nuryusuf Ali",
-		0,
-		300000,
-		"<a target=\"_blank\" href=\"{$potp}/to_m_nuryusuf.png\">Bukti transfer</a>"
-	],
-	[
-		"2024-09-02 08:56:30",
-		"Funding from Gilang Fachrezi",
-		300000,
-		0,
-		"<a target=\"_blank\" href=\"{$potp}/from_gilang_and_nyoman.jpeg\">Bukti transfer</a> (yang 50k dari Nyoman)"
-	],
-	[
-		"2024-09-02 08:56:30",
-		"Funding from I Nyoman Kahar Wedanta",
-		50000,
-		0,
-		"<a target=\"_blank\" href=\"{$potp}/from_gilang_and_nyoman.jpeg\">Bukti transfer</a> (yang 300k dari Gilang)"
-	],
-	[
-		"2024-09-02 13:21:00",
-		"Funding from I Putu Heri Subrata",
-		60000,
-		0,
-		"<a target=\"_blank\" href=\"{$potp}/from_heri.jpeg\">Bukti transfer</a>"
-	],
-	[
-		"2024-09-03 19:58:32",
-		"Funding from Made Widiambara",
-		500000,
-		0,
-		"<a target=\"_blank\" href=\"{$potp}/from_made.jpeg\">Bukti transfer</a>"
-	],
-	[
-		"2024-09-04 18:17:23",
-		"Funding from Arief Setiawan",
-		100000,
-		0,
-		"<a target=\"_blank\" href=\"{$potp}/from_arief.jpeg\">Bukti transfer</a>"
-	]
-];
+require __DIR__."/../src/udata.php";
 
 ?><!DOCTYPE html>
 <html>
